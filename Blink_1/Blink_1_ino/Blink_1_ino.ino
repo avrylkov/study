ceb89606baf36f04f13514442472f54928b0abb2
avrylkov <arylkov70@mail.ru>
2017-07-22 09:11:31 +0300

    iss_01

--- Blink_1/Blink_1_ino/Blink_1_ino.ino
+++ Blink_1/Blink_1_ino/Blink_1_ino.ino
@@ -5,6 +5,7 @@
   Turns on an LED on for one second, then off for one second, repeatedly.
  
   This example code is in the public domain.
+  iss_01
  */
  
 // Pin 13 has an LED connected on most Arduino boards.
